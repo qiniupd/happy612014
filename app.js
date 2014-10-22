544e637255408027081d8a64aa4f8c807493869f
Mc.Spring <Heresy.Mc@gmail.com>
2014-10-22 20:09:14 +0800

    Update app.js
    change default port to 19080

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var express = require('express'),
 var app = express();
 
 app.configure(function() {
-    app.set('port', process.env.PORT || 3080);
+    app.set('port', process.env.PORT || 19080);
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
     app.use(express.favicon());
